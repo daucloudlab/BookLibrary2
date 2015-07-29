0cd71148908f0cbafc9f5853f2667209d9abd29a
Daulet Baimbetov <daucloudlab@gmail.com>
2015-07-29 16:55:12 +0600

    Онлайн Библиотека (version 04) LoginValidator қолданушының есімі әріптен басталуы керегін тексереді

--- src/java/kz/kokmardan/validator/LoginValidator.java
+++ src/java/kz/kokmardan/validator/LoginValidator.java
@@ -15,13 +15,23 @@ public class LoginValidator implements Validator {
     @Override
     public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
         
-        if(value.toString().length() < 5){
-            ResourceBundle bundle = ResourceBundle.getBundle("kz.kokmardan.library.nls.messages",
+        String newvalue = value.toString() ;
+        ResourceBundle bundle = ResourceBundle.getBundle("kz.kokmardan.library.nls.messages",
             FacesContext.getCurrentInstance().getViewRoot().getLocale()) ; 
+        
+        if(newvalue.length() < 5){
             FacesMessage message = new FacesMessage(bundle.getString("login_length_error")) ;
             message.setSeverity(FacesMessage.SEVERITY_ERROR);
             throw new ValidatorException(message) ;
-    }
+        }
+        
+        if(!Character.isLetter(newvalue.charAt(0))){
+            FacesMessage message2 = new FacesMessage(bundle.getString("first_letter_error")) ;
+            message2.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(message2) ;
+        }
+        
+        
     
     }
     
