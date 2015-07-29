bfac750f852e8e65c96ac631af7451965b298aa2
Daulet Baimbetov <daucloudlab@gmail.com>
2015-07-29 13:03:37 +0600

    Онлайн Библиотека (version 01) welcome файл дайын

---
+++ src/java/kz/kokmardan/beans/User.java
@@ -0,0 +1,26 @@
+package kz.kokmardan.beans;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class User implements Serializable{
+
+    private String username ;
+    
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    
+    
+}
